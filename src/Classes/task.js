7addc8544f6364aa8430faabbb97cb0705a515c5
2fer1 <dawsonkaterberg@gmail.com>
2025-05-10 23:30:30 -0400

    Made constructor for Tasks

---
+++ src/Classes/task.js
@@ -0,0 +1,9 @@
+class Task{
+    constructor(start, end, title, description, importance){
+        this.start = start;
+        this.end = end;
+        this.title = title;
+        this.description = description;
+        this.importance = importance;
+    }
+}
